7252235aceece94405bc48fd51417225929e9199
Russell Lubojacky <russell.lubojacky@gmail.com>
2023-08-24 17:15:32 -0500

    adding openapi docs

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,31 @@ import cors from 'cors';
 import trailRoutes from './routes/trailRouter';
 import sequelize from './db/config';
 import helmet from 'helmet';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Trails API',
+            version: '1.0.0',
+            description: 'API documentation for the Trails application'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/api'
+            }
+        ]
+    },
+    apis: ['./src/routes/*.ts'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(cors());
 app.use(helmet());
--- src/routes/trailRouter.ts
+++ src/routes/trailRouter.ts
@@ -3,9 +3,111 @@ import { getAllTrails, createTrail, getTrailById, deleteTrail } from '../control
 
 const trailRouter = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Trail:
+ *          type: object
+ *          required: 
+ *              - name
+ *              - difficulty
+ *          properties:
+ *              id:
+ *                  type: number
+ *                  description: The auto-generated id of the trail
+ *              name:
+ *                  type: string
+ *                  description: The name of the trail
+ *              difficulty:
+ *                  type: string
+ *                  description: The difficulty of the trail
+ *              length:
+ *                  type: number
+ *                  description: The length of the trail
+ *          example:
+ *              id: 1
+ *              name: 'Forest Trail'
+ *              difficulty: 'Hard'
+ *              length: 10
+ */
+
+/**
+ * @swagger
+ * /trails:
+ *  get:
+ *      summary: Retrieve a list of trails
+ *      tags: [Trails]
+ *      responses:
+ *          200:
+ *              description: A successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Trail'
+ */
 trailRouter.get('/', getAllTrails);
+
+/**
+ * @swagger
+ * /trails:
+ *  post:
+ *      summary: Creates a new trail
+ *      tags: [Trails]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     $ref: '#/components/schemas/Trail' 
+ *      responses:
+ *          201:
+ *              description: A successful response
+ */
 trailRouter.post('/', createTrail);
+
+/**
+ * @swagger
+ * /trails/{id}:
+ *  get:
+ *      summary: Retrieve a specific trail by its ID
+ *      tags: [Trails]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: ID of the trail
+ *            schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: A successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Trail'
+ */
 trailRouter.get('/:id', getTrailById);
+
+/**
+ * @swagger
+ * /trails:
+ *  delete:
+ *      summary: Deletes a trail
+ *      tags: [Trails]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: ID of the trail
+ *            schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: A successful response
+ */
 trailRouter.delete('/:id', deleteTrail)
 
 export default trailRouter;
